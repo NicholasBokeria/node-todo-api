f3cf863f9c4cc5fb5cc9f62ac0c4784270fc102d
NicholasBokeria <nikolozbokeria01@gmail.com>
2018-12-01 21:00:50 +0400

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -224,3 +224,58 @@ describe('POST /users', () => {
             .end(done())
     })
 })
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', done => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[0].email,
+                password: users[0].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist()
+            })
+            .end((err, res) => {
+                if(err) {
+                    return
+                }
+
+                User.findById(users[0]._id)
+                    .then(user => {
+                        expect(user.tokens[0]).toInclude({
+                            access: 'auth',
+                            token: res.headers['x-auth']
+                        })
+                        done()
+                    }).catch(e => done(e))
+                done()
+            })
+    })
+
+    it('should reject invalid login', done => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[0].email,
+            password: users[0].password
+        })
+        .expect(400)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toNotExist()
+        })
+        .end((err, res) => {
+            if(err) {
+                return
+            }
+
+            User.findById(users[0]._id)
+                .then(user => {
+                    expect(user.tokens.length).toBe(0)
+                    done()
+                }).catch(e => done(e))
+            done()
+        })
+    })
+})
