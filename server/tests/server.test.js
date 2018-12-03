c43c23dafb0e4127f2b390ac19433e7c11a181ba
NicholasBokeria <nikolozbokeria01@gmail.com>
2018-12-03 18:58:44 +0400

    Add tests fro DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -279,3 +279,20 @@ describe('POST /users/login', () => {
         })
     })
 })
+
+describe('DELETE /users/me/token', () => {
+    it('should delete token on logout', done => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err) return
+
+                User.findById(users[0]._id)
+                    .then(user => {
+                        expect(user.tokens.length).toBe(0)
+                    }).catch(err => console.log(err))
+            })
+    })
+})
